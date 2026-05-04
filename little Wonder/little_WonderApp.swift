fbfda964e362edbd5578b459120222e677031928
Jathniel Argueza <jathnielargueza@gmail.com>
2026-05-04 10:12:22 +0800

    Initial Commit

---
+++ little Wonder/little_WonderApp.swift
@@ -0,0 +1,17 @@
+//
+//  little_WonderApp.swift
+//  little Wonder
+//
+//  Created by Jathniel Argueza on 5/4/26.
+//
+
+import SwiftUI
+
+@main
+struct little_WonderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
